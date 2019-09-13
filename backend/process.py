dcdf16562b7b43255c8e4af63c235041c57906ff
Nirmal <nirmal_kumar@live.in>
2019-09-13 22:12:48 +0530

    Implemented Conversion

--- backend/process.py
+++ backend/process.py
@@ -1,17 +1,83 @@
 from PIL import Image
 from google.cloud import pubsub_v1
 import time
+import json
+import pathlib
+import os
+from google.auth import jwt
+from google.cloud import storage
 
 project_id="bk-projects"
 subscription_name="image-subscribers"
+bucket_name="photomgr"
 
-subscriber = pubsub_v1.SubscriberClient()
+service_account_info = json.load(open("creds.json"))
+audience = "https://pubsub.googleapis.com/google.pubsub.v1.Subscriber"
+
+credentials = jwt.Credentials.from_service_account_info(
+    service_account_info, audience=audience
+)
+
+subscriber = pubsub_v1.SubscriberClient(credentials=credentials)
 subscription_path = subscriber.subscription_path(
     project_id, subscription_name)
 
+
+
+def convert_image(input_image_path,
+    output_image_path):
+   color_image = Image.open(input_image_path)
+   bw = color_image.convert('L')
+   bw.save(output_image_path)
+   #converted
+
+
+def _get_storage_client():
+      return storage.Client.from_service_account_json('creds.json')
+
+def process_image(filepath):
+    storage_client=_get_storage_client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(filepath)
+
+    print("Dowloading file to Local VM instance for processing")
+    destination_file_name="/tmp/" + filepath
+    local_dir_name=os.path.dirname(destination_file_name)
+    pathlib.Path(local_dir_name).mkdir(parents=True, exist_ok=True)
+    blob.download_to_filename(destination_file_name)
+    converted_path="/tmp/converted/" + filepath
+    local_dir_name=os.path.dirname(converted_path)
+    pathlib.Path(local_dir_name).mkdir(parents=True, exist_ok=True)
+    convert_image(destination_file_name,converted_path)
+
+    #Write the conversion logic later
+    return converted_path
+
+
+
+def save_processed_file_to_cloud(filepath, filename):
+    client = _get_storage_client()
+    bucket = client.bucket(bucket_name)
+    fileparts=filename.split("/")
+    actual_filename=os.path.join(fileparts[0],"converted",fileparts[1])
+    # req-22/converted/new-rabbit.jpg
+    print("Uploading file to blob")
+    print(actual_filename)
+    blob = bucket.blob(actual_filename)
+    blob.upload_from_filename(filepath)
+
+    return actual_filename
+
 def callback(message):
+
     print('Received message: {}'.format(message))
+    encoding = 'utf-8'
+    blob_filepath=message.data.decode(encoding)
+    final_processed=process_image(blob_filepath)
+    save_processed_file_to_cloud(final_processed, blob_filepath)
     message.ack()
+
+
 
 subscriber.subscribe(subscription_path, callback=callback)
 
@@ -20,14 +86,6 @@ print('Listening for messages on {}'.format(subscription_path))
 while True:
     time.sleep(60)
 
-
-
-def black_and_white(input_image_path,
-    output_image_path):
-   color_image = Image.open(input_image_path)
-   bw = color_image.convert('L')
-   bw.save(output_image_path)
-   #converted
 
 
 
--- frontend/main.py
+++ frontend/main.py
@@ -1,18 +1,17 @@
 import os
 import random
 from google.cloud import storage
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for,send_file
 # [START gae_python37_app]
 import flask
 import six
+from flask import jsonify
 from werkzeug.utils import secure_filename
+import tempfile
 
-UPLOAD_FOLDER = '/tmp/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 
 
 #Google Cloud Utility Functions
@@ -27,11 +26,10 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def save_file_to_cloud(file_stream, filename, content_type):
+def save_file_to_cloud(randomRequestId,file_stream, filename, content_type):
     print("Uploading file to cloud")
     client = _get_storage_client()
     bucket = client.bucket("photomgr")
-    randomRequestId=random.randint(1, 101)
     actual_filename=f"req-{randomRequestId}/{filename}"
     blob = bucket.blob(actual_filename)
     blob.upload_from_string(
@@ -60,6 +58,44 @@ def send_message_to_topic(actualBlob_path):
     topic='image-process-topic')
   publisher.publish(topic_name, str.encode(actualBlob_path))
 
+
+def renderFromBlobStorage(filename):
+    client = _get_storage_client()
+    bucket = client.get_bucket("photomgr")
+    blob = bucket.blob(filename)
+    with tempfile.NamedTemporaryFile() as temp:
+        blob.download_to_filename(temp.name)
+        return send_file(temp.name, attachment_filename=filename,mimetype='image/jpeg')
+
+
+@app.route('/render-image')
+def render_image():
+      req_id=request.args.get("req_id")
+      filename=request.args.get("filename")
+      mode=request.args.get("mode")
+      if(mode=="p"):
+        filename=f"req-{req_id}/converted/{filename}"
+      if(mode=="o"):
+        filename=f"req-{req_id}/{filename}"
+      return renderFromBlobStorage(filename)
+
+def blob_exists(bucket_name, filename):
+  client = _get_storage_client()
+  bucket = client.get_bucket(bucket_name)
+  blob = bucket.blob(filename)
+  return blob.exists()
+
+@app.route('/api/check-status', methods=['GET'])
+def check_status():
+      req_id=request.args.get("req_id")
+      filename=request.args.get("filename")
+      # GO TO GOOGLE STORAGE and check whether the converted folder exists
+      actual_filename=f"req-{req_id}/converted/{filename}"
+      is_converted=blob_exists("photomgr",actual_filename)
+      process_status={"processed":is_converted,"req_id":req_id,"filename":filename}
+      return jsonify(process_status)
+
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -76,11 +112,13 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            blob_filename=save_file_to_cloud(file.read(),filename, file.content_type)
+            randomRequestId=random.randint(1, 101)
+            blob_filename=save_file_to_cloud(randomRequestId,file.read(),filename, file.content_type)
             #send a message to the Topic
             send_message_to_topic(blob_filename)
-            #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return "file uploaded. Please wait for sometime for the file to get processed."
+
+            template_values = {"filename":file.filename, "request_id":randomRequestId}
+            return flask.render_template('index.html', **template_values)
 
 
 #if __name__ == '__main__':
